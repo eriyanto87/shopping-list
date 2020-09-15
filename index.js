fcf61d35e59be9f8fa5ccf43dcbb8b80d065e0de
Evisari Riyanto <evisari111012@gmail.com>
2020-09-14 20:16:04 -0700

    added interactivity to my page. addItem(), deleteItem(), check/uncheckItems()

--- index.js
+++ index.js
@@ -4,20 +4,27 @@ function addItems() {
     $("ul").append(
         `<li> <span class="shopping-item"> ${$("#shopping-list-entry").val()} </span> <div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div> </li>`
     );
+    deleteItems();
+    checkedItems();
   });
 }
 
 function deleteItems() {
-    $('.shopping-item-delete').on('click', 'li', function(e) {
-        e.stopPropagation()
-        this.closest('li').remove();
-      });
-   /* $('.shopping-item-delete').click(function(e) {
-        e.stopPropagation()
-        console.log(this.closest('li'))
-        this.closest('li').remove()
-    })*/
+        $('.shopping-item-delete').on('click', function() {
+          this.closest('li').remove();
+        });
+    }
+
+function checkedItems() {
+    $('.shopping-item-toggle').on('click', function () {
+        $(this)
+          .closest('li')
+          .find('.shopping-item')
+          .toggleClass('shopping-item__checked');
+      })
 }
+
 
 addItems();
 deleteItems();
+checkedItems();
